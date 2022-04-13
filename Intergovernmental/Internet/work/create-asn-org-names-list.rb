dc75c9e21c849db39e58f9e31c2692408866c1bb
Robb Shecter <robb@public.law>
2022-04-12 20:31:32 -0600

    finished the script

--- Intergovernmental/Internet/work/create-asn-org-names-list.rb
+++ Intergovernmental/Internet/work/create-asn-org-names-list.rb
@@ -4,7 +4,22 @@
 def get_org_name(asn)
   whois_data      = `whois #{asn}`.split("\n")
   org_name_record = whois_data.select{|r| r =~ /^OrgName:/}.first
+  return '' if org_name_record.nil?
+
   org_name        = org_name_record.split(':').last.strip
 end
 
-puts get_org_name('14593')
+
+def output_new_record(asn, name, org_name)
+  puts("#{asn}\t#{name}\t#{org_name}")
+end
+
+
+asn_records = File.open('../all-asns.tsv').readlines
+asn_records.each do |record|
+  country, asn, name = record.strip.split("\t")
+  next if name == '-Reserved AS-'
+  
+  org_name = get_org_name(asn)
+  output_new_record(asn, name, org_name)
+end
